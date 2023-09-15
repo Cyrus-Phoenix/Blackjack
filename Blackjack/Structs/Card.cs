b330821a436d887db07544773c282a619ba6e7db
Reza <Cyrus@DESKTOP>
2023-09-15 13:48:55 +0200

    Add project files.

---
+++ Blackjack/Structs/Card.cs
@@ -0,0 +1,27 @@
+﻿namespace Blackjack.Structs
+{
+    public struct Card
+    {
+        const string _back = "🂠";
+        string _symbol = string.Empty;
+
+        public bool IsHidden { get; set; }
+
+        public string Symbol { get { return IsHidden ? _back : _symbol; } }
+
+        // Samma som ovan fast mer modernare sätt att skriva den på
+        int _value = 0;
+        public int Value => IsHidden ? _value : 0;
+
+        public bool IsRemoved { get; set; }
+        public Card(string symbol, int value, bool isHidden = false)
+        {
+            _symbol = symbol;
+            _value = value;
+            IsHidden = isHidden;
+
+        }
+
+
+    }
+}
